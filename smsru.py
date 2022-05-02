4321e99502f2ee63ab9aaa35be1bf1b20a5b9a16
XpycTee <shift1998@bk.ru>
2022-05-02 22:48:04 +0500

    Documentation

--- smsru.py
+++ smsru.py
@@ -20,10 +20,11 @@ class OutOfTimestamp(Exception):
 
 class ABCSmsRu:
     """ SMS.RU API class
-            :param api_id: Ваш API ключ на главной странице личного кабинета """
+
+        :param api_id: Ваш API ключ на главной странице личного кабинета """
     __metaclass__ = ABCMeta
 
-    def __init__(self, api_id):
+    def __init__(self, api_id: str):
         self._debug_status = False
         self._api_id = api_id
         self._data = {'api_id': self.api_id, 'json': 1, 'partner_id': 358434}
@@ -37,32 +38,98 @@ class ABCSmsRu:
         return self._data
 
     @abstractmethod
-    def _request(self, path, data):
+    def _request(self, path: str, data: dict):
         """ Запрос на сревер
+
             :param path: путь для отправки данных
             :param data: данные для отправки на сервер
-            :return response: Ответ от сервера """
+            :return: JSON ответ от сервера """
         pass
 
     @abstractmethod
     def send(self, numbers: list[str], message: str, from_name: str, ip_address: str, timestamp: int,
              ttl: int, day_time: bool, translit: bool, test: bool, debug: bool):
         """ Отправка сообщения на сервер SMS.RU
-                :param numbers: Номер телефона получателя (либо несколько номеров до
+
+            :param numbers: Номер телефона получателя (либо несколько номеров до
                 100 штук за один запрос). Номер телефона для отправки сообщения, желатьельно без кода страны. Возможно
                 исполльзования и других видов, скрипт удалит все не нужное.
-                :param message: Текст сообщения в кодировке UTF-8 .
-                :param from_name: [Опционально] Имя отправителя (должно быть согласовано с администрацией).
-                :param ip_address: [Опционально] В этом параметре вы можете передать нам  IP адрес вашего пользователя.
-                :param timestamp: [Опционально] Время отложенной отправки.
-                :param ttl: [Опционально] Срок жизни сообщения в минутах (от 1 до 1440).
-                :param day_time: [Опционально] Учитывает часовой пояс получателя. Если указан этот параметр,
+            :param message: Текст сообщения в кодировке UTF-8 .
+            :param from_name: [Опционально] Имя отправителя (должно быть согласовано с администрацией).
+            :param ip_address: [Опционально] В этом параметре вы можете передать нам  IP адрес вашего пользователя.
+            :param timestamp: [Опционально] Время отложенной отправки.
+            :param ttl: [Опционально] Срок жизни сообщения в минутах (от 1 до 1440).
+            :param day_time: [Опционально] Учитывает часовой пояс получателя. Если указан этот параметр,
                 то параметр time игнорируется.
-                :param test: [Опционально] Имитирует отправку сообщения для тестирования. True или False
-                :param translit: [Опционально] Переводит все русские символы в латинские.
-                :param debug: [Опционально] Включает режим отладки. Все собщения отправляются с парпметром test: True
+            :param test: [Опционально] Имитирует отправку сообщения для тестирования. True или False
+            :param translit: [Опционально] Переводит все русские символы в латинские.
+            :param debug: [Опционально] Включает режим отладки. Все собщения отправляются с парпметром test: True
                 если он не указан в ручную
-                :return response: Ответ от сервера """
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def status(self, sms_id: str):
+        """ Получение стстуса СМС по id
+
+            :param sms_id: id СМС в сиситеме sms.ru
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def cost(self, numbers: list[str], message: str):
+        """ Получение стоимости смс
+
+             :param numbers: Номер телефона получателя (либо несколько номеров до
+                100 штук за один запрос). Номер телефона для отправки сообщения, желатьельно без кода страны. Возможно
+                исполльзования и других видов, скрипт удалит все не нужное.
+            :param message: Текст сообщения в кодировке UTF-8 .
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def balance(self):
+        """ Получение баланса аккаунта
+
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def limit(self):
+        """ Узнать лимит
+
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def senders(self):
+        """ Получить одобренных отправителей
+
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def stop_list(self):
+        """ Получить список номеров в стоплисте
+
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def add_stop_list(self, number: str, comment: str):
+        """ Добавить телефон в стоп лист
+
+            :param number: Номер для добавления
+            :param comment: Комментарий
+            :return: JSON ответ от сервера """
+        pass
+
+    @abstractmethod
+    def del_stop_list(self, number: str):
+        """ Удалить телефон из стоп листа
+
+            :param number: Номер для удаления
+            :return: JSON ответ от сервера """
         pass
 
     def _collect_data(self, numbers, message,
