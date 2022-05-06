4b8fbf52f6252dcfe8f51697ed3767d2cd1b64b2
XpycTee <i@xpyctee.ru>
2022-05-06 10:47:51 +0500

    Fix installing for pip

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 with open("README.md", "r", encoding='UTF-8') as f:
     readme = f.read()
 
 
-def requirements():
-    with open("requirements.txt", "r") as req:
-        return [r for r in req.read().split("\n") if r]
-
+requirements = ['aiohttp~=3.8.1']
 
 setup(name='smsru_api',
-      version='0.1.2',
+      version='0.1.3',
       description='Python API для сервиса отправки сообщений sms.ru',
       url="https://git.xpyctee.ru/XpycTee/SmsRU_API",
       long_description=readme,
       long_description_content_type="text/markdown",
       license='GNU General Public License v3.0',
-      packages=['smsru_api'],
+      packages=find_packages(),
       author="XpycTee",
       author_email='i@xpyctee.ru',
       keywords="sms.ru api sms ru",
-      install_requires=requirements(),
-      zip_safe=False
-)
+      install_requires=requirements,
+      python_requires='>=3.8',
+      )
