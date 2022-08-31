7dc2541956b6bda3ff922b3252a04c95c4a614a8
XpycTee <i@xpyctee.ru>
2022-08-31 15:37:43 +0500

    Update

--- setup.py
+++ setup.py
@@ -5,19 +5,31 @@ with open("README.md", "r", encoding='UTF-8') as f:
     readme = f.read()
 
 
-requirements = ['aiohttp~=3.8.1']
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries",
+]
 
-setup(name='smsru_api',
-      version='0.1.6',
+
+setup(
+      name='smsru_api',
+      version='0.1.7',
       description='Python API для сервиса отправки сообщений sms.ru',
-      url="https://git.xpyctee.ru/XpycTee/SmsRU_API",
+      url="https://github.com/XpycTee/smsru_api",
       long_description=readme,
       long_description_content_type="text/markdown",
       license='Apache License 2.0',
       packages=find_packages(),
       author="XpycTee",
       author_email='i@xpyctee.ru',
+      classifiers=classifiers,
       keywords="sms.ru api sms ru",
-      install_requires=requirements,
-      python_requires='>=3.8',
-      )
+      install_requires=['aiohttp'],
+      python_requires='>=3.7',
+)
