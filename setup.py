8f70b83598fbf63496c536374ed41e1ae0aab692
XpycTee <i@xpyctee.ru>
2025-03-18 15:16:55 +0500

    Refactor setup.py for improved readability and formatting

--- setup.py
+++ setup.py
@@ -22,18 +22,21 @@ classifiers = [
 
 
 setup(
-      name='smsru_api',
-      version='0.2',
-      description='Python API для сервиса отправки сообщений sms.ru',
-      url="https://github.com/XpycTee/smsru_api",
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      license='Apache License 2.0',
-      packages=find_packages(),
-      author="XpycTee",
-      author_email='i@xpyctee.ru',
-      classifiers=classifiers,
-      keywords="sms.ru api sms ru",
-      install_requires=['aiohttp', 'certifi'],
-      python_requires='>=3.8',
+    name='smsru_api',
+    version='0.2',
+    description='Python API для сервиса отправки сообщений sms.ru',
+    url="https://github.com/XpycTee/smsru_api",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    packages=find_packages(),
+    author="XpycTee",
+    author_email='i@xpyctee.ru',
+    classifiers=classifiers,
+    keywords="sms.ru api sms ru",
+    install_requires=[
+        'aiohttp', 
+        'certifi'
+    ],
+    python_requires='>=3.8',
 )
